a02d4ebf1bf6fde1fad5d2142f8ce48b3061479c
kaiser <1244713586@qq.com>
2018-12-24 00:36:14 +0800

    2018.12.23

--- chapter14/ex_14_44.cpp
+++ chapter14/ex_14_44.cpp
@@ -4,7 +4,24 @@
 
 #include <functional>
 #include <iostream>
+#include <unordered_map>
+#include <string>
+#include <cstdint>
 
 int main() {
 
+  std::unordered_map<std::string,
+                     std::function<std::int32_t(std::int32_t, std::int32_t)>> binops{
+      {"+", std::plus<>{}},
+      {"-", std::minus<>{}},
+      {"*", std::multiplies<>{}},
+      {"/", std::divides<>{}},
+  };
+
+  std::string op;
+  std::int32_t lhs, rhs;
+
+  // 注意要输入空格
+  std::cin >> lhs >> op >> rhs;
+  std::cout << binops[op](lhs, rhs) << '\n';
 }
