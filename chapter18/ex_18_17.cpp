0fff97a253a1d3814233db05e5b331fc6e9802a4
kaiser <1244713586@qq.com>
2019-03-15 21:10:41 +0800

    2019.3.15

--- chapter18/ex_18_17.cpp
+++ chapter18/ex_18_17.cpp
@@ -2,3 +2,22 @@
 // Created by kaiser on 19-3-14.
 //
 
+#include <iostream>
+
+namespace exercise {
+int ivar = 10;
+double dvar = 0;
+const int limit = 1000;
+}  // namespace exercise
+
+int ivar = 0;
+
+// using exercise::ivar, exercise::dvar, exercise::limit;
+
+int main() {
+  using exercise::ivar, exercise::dvar, exercise::limit;
+  // double dvar = 3.1416;
+  int iobj = limit + 1;
+  std::cout << ivar << '\n';
+  std::cout << ::ivar << '\n';
+}
