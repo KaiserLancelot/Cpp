5f13c3968b31c5feb5c1f46d5f461ac2bd4303ff
kaiser <1244713586@qq.com>
2019-03-19 17:51:16 +0800

    2019.3.19

---
+++ cpp11-14-17/constexpr_if.cpp
@@ -0,0 +1,31 @@
+//
+// Created by kaiser on 19-3-19.
+//
+
+// https://stackoverflow.com/questions/43434491/difference-between-if-constexpr-vs-if
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+template <typename T>
+constexpr auto length(const T& value) noexcept {
+  // C++17 std::is_integral_v
+  // 检查 T 是否是整数类型
+  // C++17 constexpr-if
+  // 在编译时判断, 编译后不存在分支
+  if constexpr (std::is_integral_v<T>) {
+    return value;
+  } else {
+    return std::size(value);
+  }
+}
+
+int main() {
+  std::int32_t a{42};
+  std::string b("foo");
+  std::cout << length(a) << '\n' << length(b) << '\n';
+  // false
+  std::cout << std::boolalpha << std::is_integral_v<int&> << '\n';
+}
---
+++ notes/odr.md
@@ -0,0 +1,2 @@
+单一定义规则 One Definition Rule
+[参见](https://zh.cppreference.com/w/cpp/language/definition)
