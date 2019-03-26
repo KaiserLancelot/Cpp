fc7cc0eb8a6e216a68ead05ac215c9aadbb6a934
kaiser <1244713586@qq.com>
2019-03-26 11:58:34 +0800

    2019.3.26

---
+++ library/Qt/enum.h
@@ -0,0 +1,32 @@
+
+//
+// Created by kaiser on 19-3-20.
+//
+
+#ifndef CPP_PRIMER_ENUM_H
+#define CPP_PRIMER_ENUM_H
+
+#include <QMetaEnum>
+#include <QObject>
+#include <string>
+
+class Enum : public QObject {
+ Q_OBJECT
+ public:
+  enum Values { kTest };
+  Q_ENUM(Values);
+};
+
+class Enum2 : public QObject {
+ Q_OBJECT
+ public:
+  enum Values { kTest2 };
+  Q_ENUM(Values);
+};
+
+template <typename T>
+std::string ToString(T value) {
+  return QMetaEnum::fromType<T>().valueToKey(value) + 1;
+}
+
+#endif  // CPP_PRIMER_ENUM_H
