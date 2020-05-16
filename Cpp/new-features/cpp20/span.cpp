d13c2359e6396d2f317bbc2099b35b01d1e84f85
KaiserLancelot <1244713586@qq.com>
2020-05-16 08:23:02 +0800

    2020.5.16

---
+++ Cpp/new-features/cpp20/span.cpp
@@ -0,0 +1,37 @@
+/**
+ * @ Author: KaiserLancelot
+ * @ Create Time: 2020-05-16 07:33:22
+ * @ Modified time: 2020-05-16 08:18:56
+ */
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <ranges>
+#include <span>
+#include <vector>
+
+// 类模板 span 所描述的对象能指代对象的相接序列, 序列的首元素在零位置
+// span 能拥有静态长度, 该情况下序列中的元素数已知并编码于类型中, 或拥有动态长度
+// 典型实现只保有二个成员: 指向 T 的指针和大小
+
+void Fun(std::span<std::int32_t> span) {
+  std::cout << span.front() << '\n';
+  std::cout << span.back() << '\n';
+  std::cout << span[2] << '\n';
+
+  // 获得子段
+  std::ranges::sort(span.subspan(0, 3));
+  // span.first(3) 前 N 个元素组成的子段
+  // span.last(3)
+}
+
+int main() {
+  std::vector<std::int32_t> v{11, 2, 1, 434, 22, 3};
+  Fun(v);
+
+  for (auto i : v) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+}
