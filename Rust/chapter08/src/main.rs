cea0a1d9d1abba9fc2fc8e3e5d9f537274cada60
KaiserLancelot <1244713586@qq.com>
2020-05-20 13:26:41 +0800

    2020.5.20

--- Rust/chapter08/src/main.rs
+++ Rust/chapter08/src/main.rs
@@ -1,7 +1,7 @@
 /**
  * @ Author: KaiserLancelot
  * @ Create Time: 2020-05-18 12:24:04
- * @ Modified time: 2020-05-18 12:41:24
+ * @ Modified time: 2020-05-20 13:26:27
  */
 
 fn main() {
@@ -52,4 +52,44 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+
+    // to_string 方法能用于任何实现了 Display trait 的类型
+    let mut s = "str".to_string();
+    s.push('3');
+    s.push_str("333");
+    s += "aa";
+    println!("{}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    // 注意 s1 被移动了, 不能继续使用(必须是下面的形式)
+    // + 运算符使用了 add 函数
+    // 类似于 fn add(self, s: &str) -> String
+    // &String 可以被强转(coerced)成 &str
+    // 当add函数被调用时, Rust 使用了一个被称为解引用强制多态(deref coercion)
+    // 的技术, 可以将其理解为它把 &s2 变成了 &s2[..]
+    // 会获取 s1 的所有权, 附加上从 s2 中拷贝的内容, 并返回结果的所有权
+    let s3 = s1 + &s2;
+    println!("{}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    // 不会获取任何参数的所有权
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    // String 不支持索引
+    // String 是一个 Vec<u8> 的封装
+    // 另一个 Rust 不允许使用索引获取 String 字符的原因是
+    // 索引操作预期总是需要常数时间.但是对于 String 不可能保证这样的性能
+    // 因为 Rust 必须从开头到索引位置遍历来确定有多少有效的字符
+
+    // 单独的 Unicode 标量值
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+    // 每一个原始字节
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
 }
