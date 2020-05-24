5ce0301846941069cd0739a4fe353c165ebb99f3
KaiserLancelot <1244713586@qq.com>
2020-05-24 23:27:38 +0800

    2020.5.24

--- Cpp/library/database/leveldb/main.cpp
+++ Cpp/library/database/leveldb/main.cpp
@@ -1,7 +1,47 @@
 /**
  * @ Author: KaiserLancelot
  * @ Create Time: 2020-05-24 23:00:24
- * @ Modified time: 2020-05-24 23:00:28
+ * @ Modified time: 2020-05-24 23:25:56
  */
 
-int main() {}
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <leveldb/db.h>
+#include <leveldb/write_batch.h>
+
+int main() {
+  leveldb::DB* p;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  // options.error_if_exists = true;
+  auto status{leveldb::DB::Open(options, "testdb", &p)};
+  // 不使用智能指针的话需要手动 delete
+  std::unique_ptr<leveldb::DB> db{p};
+
+  if (!status.ok()) {
+    std::cerr << status.ToString() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  leveldb::WriteBatch batch;
+  batch.Put("name", "kaiser");
+  status = db->Write(leveldb::WriteOptions(), &batch);
+  if (!status.ok()) {
+    std::cerr << status.ToString() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  std::string value;
+  status = db->Get(leveldb::ReadOptions(), "name", &value);
+  if (!status.ok()) {
+    std::cerr << status.ToString() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  assert(value == "kaiser");
+  std::cout << value << '\n';
+}
