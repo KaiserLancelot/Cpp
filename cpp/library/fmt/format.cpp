a751f3fa22e20cb6e09b012ed846c0708b7713a3
kaiser <1244713586@qq.com>
2020-08-17 21:42:11 +0800

    2020.8.17

--- cpp/library/fmt/format.cpp
+++ cpp/library/fmt/format.cpp
@@ -14,7 +14,7 @@ int main() {
   // fmt 构造一个编译时格式字符串,默认此宏被禁用以防止名字冲突,
   // 要使用它需要先将 FMT_STRING_ALIAS 定义为 1
   // 如果说明符无效则编译错误
-  std::string s(fmt::format(fmt("{:s}"), "foo"));
+  std::string s(fmt::format(FMT_STRING("{:s}"), "foo"));
   fmt::print("{}\n", s);
 
   // 返回字符数
