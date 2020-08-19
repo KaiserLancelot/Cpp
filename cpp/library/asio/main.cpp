4ae6cbb6be7f20d766b6c795b63e8b7c1b8fe5dd
kaiser <1244713586@qq.com>
2020-08-19 18:22:11 +0800

    2020.8.19

---
+++ cpp/library/asio/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by kaiser on 2020/4/27.
+//
+
+// TODO Learn asio
+
+#include <chrono>
+#include <iostream>
+
+#include <asio.hpp>
+
+int main() {
+  using namespace std::chrono_literals;
+
+  asio::io_context ioc{1};
+  asio::steady_timer timer{ioc, 1s};
+
+  timer.async_wait([](asio::error_code error) {
+    if (error) {
+      std::cerr << error.message() << '\n';
+    } else {
+      std::cout << "Hello, world!\n";
+    }
+  });
+
+  ioc.run();
+}
