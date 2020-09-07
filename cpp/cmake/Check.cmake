0f362a98df12723328d4754af5bc099573f78a22
kaiser <1244713586@qq.com>
2020-09-08 03:31:38 +0800

    2020.9.8

--- cpp/cmake/Check.cmake
+++ cpp/cmake/Check.cmake
@@ -1,18 +1,24 @@
-if(NOT (CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME STREQUAL
-                                              "Darwin"))
-  message(FATAL_ERROR "Only supports Linux system and macOS")
-endif()
-
-if(NOT (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES
-                                                "(Apple)?[Cc]lang"))
-  message(FATAL_ERROR "Only supports GCC and Clang")
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  if(CMAKE_COMPILER_IS_GNUCXX)
+    message(STATUS "Linux and GCC")
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    message(STATUS "Linux and Clang")
+  endif()
+elseif(APPLE AND CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+  message(STATUS "macOS and AppleClang")
+elseif(WIN32 AND MSVC)
+  message(STATUS "Windows and MSVC")
+else()
+  message(FATAL_ERROR "The system and compiler combination does not support")
 endif()
 
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
   message(FATAL_ERROR "In-source builds not allowed")
 endif()
 
-if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to 'Release' as none was specified")
-  set(CMAKE_BUILD_TYPE "Release")
+if(NOT CMAKE_BUILD_TYPE)
+  message(STATUS "Setting build type to Release as none was specified")
+  set(CMAKE_BUILD_TYPE
+      "Release"
+      CACHE STRING "Choose Release or Debug" FORCE)
 endif()
--- cpp/cmake/CompilerOptions.cmake
+++ cpp/cmake/CompilerOptions.cmake
@@ -3,18 +3,38 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror")
+if(MSVC)
+  string(APPEND CMAKE_CXX_FLAGS
+         " /nologo /EHsc /GF /errorReport:queue /FC /W4 /WX")
+else()
+  string(APPEND CMAKE_CXX_FLAGS " -Wall -Wextra -Wpedantic -Werror")
+endif()
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
-  add_link_options(-fuse-ld=lld)
+if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
+  if(NOT APPLE)
+    add_link_options(-fuse-ld=lld)
+  endif()
 
   if((CMAKE_BUILD_TYPE STREQUAL "Debug") OR (CMAKE_BUILD_TYPE STREQUAL
                                              "RelWithDebInfo"))
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstandalone-debug")
+    string(APPEND CMAKE_CXX_FLAGS " -fstandalone-debug")
+  endif()
+elseif(CMAKE_COMPILER_IS_GNUCXX)
+  # FIXME
+  if(NOT CLANG_TIDY)
+    string(APPEND CMAKE_CXX_FLAGS " -fanalyzer")
   endif()
 endif()
 
 if((CMAKE_BUILD_TYPE STREQUAL "Release") OR (CMAKE_BUILD_TYPE STREQUAL
                                              "RelWithDebInfo"))
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
+  include(CheckIPOSupported)
+  check_ipo_supported(RESULT lto_supported OUTPUT error)
+
+  if(lto_supported)
+    message(STATUS "IPO / LTO enabled")
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+  else()
+    message(FATAL_ERROR "IPO / LTO not supported: <${error}>")
+  endif()
 endif()
