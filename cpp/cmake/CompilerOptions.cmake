ac2cb3abf7891c830d93d5f7b0f211faeac47e11
kaiser <1244713586@qq.com>
2020-09-12 02:22:50 +0800

    2020.9.12

--- cpp/cmake/CompilerOptions.cmake
+++ cpp/cmake/CompilerOptions.cmake
@@ -3,10 +3,17 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-string(APPEND CMAKE_CXX_FLAGS " -Wall -Wextra -Wpedantic -Werror")
+if(MSVC)
+  string(APPEND CMAKE_CXX_FLAGS
+         " /nologo /EHsc /GF /errorReport:queue /FC /W4 /WX")
+else()
+  string(APPEND CMAKE_CXX_FLAGS " -Wall -Wextra -Wpedantic -Werror")
+endif()
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
-  add_link_options(-fuse-ld=lld)
+  if(NOT APPLE)
+    add_link_options(-fuse-ld=lld)
+  endif()
 
   if((CMAKE_BUILD_TYPE STREQUAL "Debug") OR (CMAKE_BUILD_TYPE STREQUAL
                                              "RelWithDebInfo"))
