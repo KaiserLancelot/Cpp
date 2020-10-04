c7afa628480c1c1f188620d831ee442cba64e153
kaiser <1244713586@qq.com>
2020-10-05 01:55:18 +0800

    2020.10.5

--- cpp/cmake/Options.cmake
+++ cpp/cmake/Options.cmake
@@ -1,6 +1,8 @@
 option(CPP_FORMAT "Format code using clang-format and cmake-format" OFF)
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+  option(CPP_USE_LIBCXX "Use libc++" ON)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   option(CPP_USE_LIBCXX "Use libc++" OFF)
 endif()
 
