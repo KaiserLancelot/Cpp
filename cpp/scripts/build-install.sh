2a84d44ef3f571265283028d3dbb92a4add8b8a5
kaiser <1244713586@qq.com>
2020-10-06 18:26:31 +0800

    2020.10.6

--- cpp/scripts/build-install.sh
+++ cpp/scripts/build-install.sh
@@ -3,6 +3,9 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+    sudo apt update
+    sudo apt install gcc-10 g++-10
     export CC=gcc-10
     export CXX=g++-10
     PARALLEL=$(nproc)
