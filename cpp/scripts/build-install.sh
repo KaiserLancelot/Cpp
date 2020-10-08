3453d1f8bfc75ed7e4d421220b0dd7f2ec7f4401
kaiser <1244713586@qq.com>
2020-10-09 03:20:51 +0800

    2020.10.9

--- cpp/scripts/build-install.sh
+++ cpp/scripts/build-install.sh
@@ -14,8 +14,6 @@ cd dependencies
 # zlib
 if [ ! -f "zlib-1.2.11.zip" ]; then
     wget -q https://github.com/madler/zlib/archive/v1.2.11.zip -O zlib-1.2.11.zip
-else
-    echo "Build zlib"
 fi
 unzip -q zlib-*.zip
 rm zlib-*.zip
@@ -23,14 +21,12 @@ cd zlib-*
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install zlib completed"
 
 # jemalloc
 if [ ! -f "jemalloc-5.2.1.zip" ]; then
     wget -q https://github.com/jemalloc/jemalloc/archive/5.2.1.zip -O jemalloc-5.2.1.zip
-else
-    echo "Build jemalloc"
 fi
 unzip -q jemalloc-*.zip
 rm jemalloc-*.zip
@@ -38,14 +34,12 @@ cd jemalloc-*
 ./autogen.sh
 make -j$(nproc)
 sudo make install
-
 cd ..
+echo "Build and jemalloc completed"
 
 # zstd
 if [ ! -f "zstd-1.4.5.zip" ]; then
     wget -q https://github.com/facebook/zstd/archive/v1.4.5.zip -O zstd-1.4.5.zip
-else
-    echo "Build Zstandard"
 fi
 unzip -q zstd-*.zip
 rm zstd-*.zip
@@ -54,14 +48,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DZSTD_BUILD_PROGRAMS=OFF -DZSTD_BUILD_TESTS=OFF
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ../../..
+echo "Build and install zstd completed"
 
 # rocksdb
 if [ ! -f "rocksdb-6.11.4.zip" ]; then
     wget -q https://github.com/facebook/rocksdb/archive/v6.11.4.zip -O rocksdb-6.11.4.zip
-else
-    echo "Build RocksDB"
 fi
 unzip -q rocksdb-*.zip
 rm rocksdb-*.zip
@@ -73,14 +65,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DWITH_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install rocksdb completed"
 
 # fmt
 if [ ! -f "fmt-7.0.3.zip" ]; then
     wget -q https://github.com/fmtlib/fmt/archive/7.0.3.zip -O fmt-7.0.3.zip
-else
-    echo "Build fmt"
 fi
 unzip -q fmt-*.zip
 rm fmt-*.zip
@@ -89,14 +79,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DFMT_DOC=OFF -DFMT_TEST=OFF -DBUILD_SHARED_LIBS=TRUE
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install fmt completed"
 
 # spdlog
 if [ ! -f "spdlog-1.8.1.zip" ]; then
     wget -q https://github.com/gabime/spdlog/archive/v1.8.1.zip -O spdlog-1.8.1.zip
-else
-    echo "Build spdlog"
 fi
 unzip -q spdlog-*.zip
 rm spdlog-*.zip
@@ -106,14 +94,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DSPDLOG_BUILD_SHARED=ON
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install spdlog completed"
 
 # google benchmark
 if [ ! -f "benchmark-1.5.2.zip" ]; then
     wget -q https://github.com/google/benchmark/archive/v1.5.2.zip -O benchmark-1.5.2.zip
-else
-    echo "Build google benchmark"
 fi
 unzip -q benchmark*.zip
 rm benchmark*.zip
@@ -124,14 +110,13 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DBENCHMARK_ENABLE_ASSEMBLY_TESTS=OFF -DBUILD_SHARED_LIBS=ON
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install google benchmark completed"
 
 # google test
 if [ ! -f "googletest-release-1.10.0.zip" ]; then
-    wget -q https://github.com/google/googletest/archive/release-1.10.0.zip -O googletest-release-1.10.0.zip
-else
-    echo "Build google test"
+    wget -q https://github.com/google/googletest/archive/release-1.10.0.zip \
+        -O googletest-release-1.10.0.zip
 fi
 unzip -q googletest-release-*.zip
 rm googletest-release-*.zip
@@ -140,14 +125,13 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_GMOCK=OFF -DBUILD_SHARED_LIBS=ON
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install google test completed"
 
 # mysql connector
 if [ ! -f "mysql-connector-cpp-8.0.21.zip" ]; then
-    wget -q https://github.com/mysql/mysql-connector-cpp/archive/8.0.21.zip -O mysql-connector-cpp-8.0.21.zip
-else
-    echo "Build mysql connector"
+    wget -q https://github.com/mysql/mysql-connector-cpp/archive/8.0.21.zip \
+        -O mysql-connector-cpp-8.0.21.zip
 fi
 unzip -q mysql-connector-cpp-*.zip
 rm mysql-connector-cpp-*.zip
@@ -157,14 +141,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_INSTALL_LIBDIR=lib
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install mysql connector completed"
 
 # magic enum
 if [ ! -f "magic_enum-0.6.6.zip" ]; then
     wget -q https://github.com/Neargye/magic_enum/archive/v0.6.6.zip -O magic_enum-0.6.6.zip
-else
-    echo "Build magic enum"
 fi
 unzip -q magic_enum-*.zip
 rm magic_enum-*.zip
@@ -173,14 +155,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF -DMAGIC_ENUM_OPT_BUILD_TESTS=OFF
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install magic enum completed"
 
 # json
 if [ ! -f "json-3.9.1.zip" ]; then
     wget -q https://github.com/nlohmann/json/archive/v3.9.1.zip -O json-3.9.1.zip
-else
-    echo "Build json"
 fi
 unzip -q json-*.zip
 rm json-*.zip
@@ -189,15 +169,13 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DJSON_BuildTests=OFF -DJSON_MultipleHeaders=ON
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install json completed"
 
 # protobuf
 if [ ! -f "protobuf-cpp-3.13.0.zip" ]; then
     wget -q https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-cpp-3.13.0.zip \
         -O protobuf-cpp-3.13.0.zip
-else
-    echo "Build protobuf"
 fi
 unzip -q protobuf-cpp-*.zip
 rm protobuf-cpp-*.zip
@@ -205,14 +183,13 @@ cd protobuf-*
 ./configure
 make -j$(nproc)
 sudo make install
-
 cd ..
+echo "Build and install protobuf completed"
 
 # libarchive
 if [ ! -f "libarchive-3.4.3.zip" ]; then
-    wget -q https://github.com/libarchive/libarchive/archive/v3.4.3.zip -O libarchive-3.4.3.zip
-else
-    echo "Build libarchive"
+    wget -q https://github.com/libarchive/libarchive/archive/v3.4.3.zip \
+        -O libarchive-3.4.3.zip
 fi
 unzip -q libarchive-*.zip
 rm libarchive-*.zip
@@ -226,14 +203,12 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_ACL=OFF -DENABLE_ICONV=OFF -DENABLE_TEST=OFF
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install libarchive completed"
 
 # curl
 if [ ! -f "curl-curl-7_72_0.zip" ]; then
     wget -q https://github.com/curl/curl/archive/curl-7_72_0.zip -O curl-curl-7_72_0.zip
-else
-    echo "Build curl"
 fi
 unzip -q curl-curl-*.zip
 rm curl-curl-*.zip
@@ -244,15 +219,13 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
     -DCURL_ZLIB=OFF -DCURL_ZSTD=OFF -DBUILD_TESTING=OFF
 cmake --build build --config Release -j$(nproc)
 sudo cmake --build build --config Release --target install
-
 cd ..
+echo "Build and install curl completed"
 
 # icu
 if [ ! -f "icu4c-67_1-src.tgz" ]; then
     wget -q https://github.com/unicode-org/icu/releases/download/release-67-1/icu4c-67_1-src.tgz \
         -O icu4c-67_1-src.tgz
-else
-    echo "Build icu"
 fi
 tar -xf icu4c-*-src.tgz
 rm icu4c-*-src.tgz
@@ -260,15 +233,13 @@ cd icu/source
 ./configure --enable-tests=no --enable-samples=no
 make -j$(nproc)
 sudo make install
-
 cd ../..
+echo "Build and install icu completed"
 
 # boost
 if [ ! -f "boost_1_74_0.tar.gz" ]; then
     wget -q https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz \
         -O boost_1_74_0.tar.gz
-else
-    echo "Build boost"
 fi
 tar -xf boost_*.tar.gz
 rm boost_*.tar.gz
@@ -276,8 +247,8 @@ cd boost_*
 ./bootstrap.sh
 ./b2 --toolset=gcc-10 --with-program_options
 sudo ./b2 --toolset=gcc-10 --with-program_options install
-
 cd ..
+echo "Build and install boost completed"
 
 cd ..
 
